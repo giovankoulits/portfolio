4619639e0235792a3b86bfaca8d6d506c58c6f7d
chupacambra <jovankoul@hotmail.com>
2023-09-07 16:54:04 +0300

    Added movie sorter. removed hangman

--- src/components/Progs/scriptsArray.js
+++ src/components/Progs/scriptsArray.js
@@ -8,16 +8,15 @@ const scriptsArray = [
     src: 'travel_advisor',
     link: 'https://travel-advisor-jsx3r4.netlify.app/',
   },
+  {
+    src: 'movie_sorter',
+    link: 'https://app.netlify.com/sites/giovankoulits-movie-sorter/overview',
+  },
   { src: 'e_shop', link: 'https://react-eshop-jsx234.netlify.app/' },
   { src: 'notes', link: 'https://notes-app-jsx345.netlify.app/' },
   { src: 'memory', link: 'https://memory-game-de8c8b.netlify.app/' },
   { src: 'quiz', link: 'https://quizzical-jsx123.netlify.app/' },
   { src: 'whack_a_mole', link: 'https://whack-a-mole-js123.netlify.app/' },
-
-  {
-    src: 'react_hangman',
-    link: 'https://giovankoulits-react-hangman.netlify.app/',
-  },
 ]
 
 export default scriptsArray
